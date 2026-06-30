1aa3916a10f5676f88a396310bae642e9bfc25e6
Ed Segawa <ed@newpaths.com>
2026-06-29 23:12:42 -0700

    Switch mailer from Zoho SMTP to Resend API

--- server/mailer.js
+++ server/mailer.js
@@ -1,26 +1,15 @@
-const nodemailer = require('nodemailer');
+const { Resend } = require('resend');
 
 async function sendEmail({ to, subject, body }) {
-  const transporter = nodemailer.createTransport({
-    host: process.env.ZOHO_SMTP_HOST,
-    port: 465,
-    secure: true,
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000,
-    auth: {
-      user: process.env.ZOHO_FROM_EMAIL,
-      pass: process.env.ZOHO_APP_PASSWORD
-    }
-  });
-
-  const info = await transporter.sendMail({
-    from: process.env.ZOHO_FROM_EMAIL,
+  const resend = new Resend(process.env.RESEND_API_KEY);
+  const { data, error } = await resend.emails.send({
+    from: `Smart Message Router <${process.env.ZOHO_FROM_EMAIL}>`,
     to,
     subject,
     text: body
   });
-  console.log('Mailer: message ID:', info.messageId, 'response:', info.response);
+  if (error) throw new Error(error.message);
+  console.log('Mailer: message ID:', data.id);
 }
 
 module.exports = { sendEmail };
