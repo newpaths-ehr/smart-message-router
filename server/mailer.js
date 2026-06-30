bce8474c742f1c6f3dcac884456afdf5d6902e19
Ed Segawa <ed@newpaths.com>
2026-06-29 22:53:19 -0700

    Switch SMTP to port 465 SSL for Railway compatibility

--- server/mailer.js
+++ server/mailer.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer');
 async function sendEmail({ to, subject, body }) {
   const transporter = nodemailer.createTransport({
     host: process.env.ZOHO_SMTP_HOST,
-    port: parseInt(process.env.ZOHO_SMTP_PORT),
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.ZOHO_FROM_EMAIL,
       pass: process.env.ZOHO_APP_PASSWORD
